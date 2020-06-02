741fce6acb555803411bfaa22c4d4804435bb626
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-02 20:51:57 +0530

    tooltip on card hover

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Card as AntdCard } from 'antd';
+import { Card as AntdCard, Tooltip } from 'antd';
 
 const { Meta } = AntdCard;
 
 const Card = ({ title, imageUrl }) => (
-  <AntdCard hoverable cover={<img alt="example" src={imageUrl} />}>
-    <Meta title={title} />
-  </AntdCard>
+  <Tooltip title={title} placement="bottom">
+    <AntdCard hoverable cover={<img alt="example" src={imageUrl} />}>
+      <Meta title={title} />
+    </AntdCard>
+  </Tooltip>
 );
 
 export default Card;
