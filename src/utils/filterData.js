1413207e7e0b8b6e884bd1f8955f1cedd611c003
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-02 16:24:20 +0530

    implicit return

--- src/utils/filterData.js
+++ src/utils/filterData.js
@@ -1,7 +1,6 @@
-export const filterData = (data, programType) => {
-  return data.entries
+export const filterData = (data, programType) =>
+  data.entries
     .filter(
       (entry) => entry.releaseYear >= 2010 && entry.programType === programType
     )
     .filter((entry, index) => index < 21);
-};
--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,26 +1,10 @@
-const titleAToZ = (a, b) => {
-  if (a.title > b.title) {
-    return 1;
-  } else {
-    return -1;
-  }
-};
+const titleAToZ = (a, b) => (a.title > b.title ? 1 : -1);
 
-const titleZtoA = (a, b) => {
-  if (a.title > b.title) {
-    return -1;
-  } else {
-    return 1;
-  }
-};
+const titleZtoA = (a, b) => (a.title > b.title ? -1 : 1);
 
-const releaseYearHighToLow = (a, b) => {
-  return b.releaseYear - a.releaseYear;
-};
+const releaseYearHighToLow = (a, b) => b.releaseYear - a.releaseYear;
 
-const releaseYearLowToHigh = (a, b) => {
-  return a.releaseYear - b.releaseYear;
-};
+const releaseYearLowToHigh = (a, b) => a.releaseYear - b.releaseYear;
 
 export default {
   titleAToZ,
