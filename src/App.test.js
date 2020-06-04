d2d56093f6b4204094376996f1d331bbcc1a7fb3
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-04 20:52:04 +0530

    unit tests for sorting functions using jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import utils from './utils/sort';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const array = [
+  { title: 'American Sniper', releaseYear: 2015 },
+  { title: 'Life of Crime', releaseYear: 2013 },
+];
+
+describe('sorting functions', () => {
+  test('should sort a-z', () => {
+    const res = array.sort((a, b) => utils['titleAToZ'](a, b));
+    expect(res[0].title).toBe('American Sniper');
+  });
+
+  test('should sort z-a', () => {
+    const res = array.sort((a, b) => utils['titleZToA'](a, b));
+    expect(res[0].title).toBe('Life of Crime');
+  });
+
+  test('should sort latest to oldest', () => {
+    const res = array.sort((a, b) => utils['releaseYearHighToLow'](a, b));
+    expect(res[0].releaseYear).toBe(2015);
+  });
+
+  test('should sort oldest to latest', () => {
+    const res = array.sort((a, b) => utils['releaseYearLowToHigh'](a, b));
+    expect(res[0].releaseYear).toBe(2013);
+  });
 });
